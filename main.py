3498eba7b3574575d8e8ac6fb8d771ac861e9cf2
HÁ <haidee.alvarez.c@gmail.com>
2024-09-23 10:18:55 +0100

    conditional to fill our dict
    if the continent encountered is not in the dict already, the create this structure and add it, but if it already exists, just append the data

--- main.py
+++ main.py
@@ -13,4 +13,10 @@ with open(filename, 'r') as csvfile:
         continent = line['continent']
         year = int(line['year'])
         population = int(line['population'])
-       
+       # If the continent doesn't already exists- create this structure
+        if continent not in population_per_continent:
+            population_per_continent[continent] = {'population':[], 'years':[]}
+        # Otherwise add this information to the dict
+        else:
+            population_per_continent[continent]['population'].append(population)
+            population_per_continent[continent]['years'].append(year)
